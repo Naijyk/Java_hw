206acf78f34d0582783f5465ee7c3b02cbc3f12b
Naijyk <133905330+Naijyk@users.noreply.github.com>
2023-06-11 15:54:05 +0200

    Add files via upload

---
+++ Hw2.java
@@ -0,0 +1,29 @@
+package homeWork;
+
+import static java.lang.Math.E;
+
+public class Hw2 {
+    public static void main(String[] args) {
+        // 1
+        float f = 1.5f;
+
+        System.out.println(Math.pow(E,5) + f);
+
+        // 2
+        short q = 14;
+
+        double result = Math.log(2) + q;
+
+        System.out.println(result);
+
+
+        // 3
+        int w = 1;
+
+        float r = 1.5f;
+
+        float r1 = w + r;
+
+        System.out.println(r1);
+    }
+}
