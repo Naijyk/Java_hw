82989a6d5e30f1d730bf16ab795a759910aa2358
Naijyk <133905330+Naijyk@users.noreply.github.com>
2023-07-03 11:05:35 +0200

    Add files via upload

---
+++ Hw8_cycles.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class Hw8_cycles {
+    public static void main(String[] args) {
+        int [] e = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+        for (int i = 0; i < e.length; i++) {
+            System.out.print(e[i] + " ");
+        }
+    }
+}
